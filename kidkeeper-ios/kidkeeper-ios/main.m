39d4d5e2f41be78d70964ecaba518c81746c33f8
Edward Guo <edwardplguo@gmail.com>
2015-08-15 09:50:07 -0400

    Add iOS project

---
+++ kidkeeper-ios/kidkeeper-ios/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  kidkeeper-ios
+//
+//  Created by Edward Guo on 2015-08-15.
+//  Copyright (c) 2015 Peiliang Guo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
