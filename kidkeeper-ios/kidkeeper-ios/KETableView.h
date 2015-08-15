1cde2eeac8f89544d5a061879864163f8fdb352e
Edward Guo <edwardplguo@gmail.com>
2015-08-15 09:54:56 -0400

    Add KETableView

---
+++ kidkeeper-ios/kidkeeper-ios/KETableView.h
@@ -0,0 +1,13 @@
+//
+//  KETableView.h
+//  kidkeeper-ios
+//
+//  Created by Edward Guo on 2015-08-15.
+//  Copyright (c) 2015 Peiliang Guo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KETableView : UITableView
+
+@end
---
+++ kidkeeper-ios/kidkeeper-ios/KETableView.m
@@ -0,0 +1,21 @@
+//
+//  KETableView.m
+//  kidkeeper-ios
+//
+//  Created by Edward Guo on 2015-08-15.
+//  Copyright (c) 2015 Peiliang Guo. All rights reserved.
+//
+
+#import "KETableView.h"
+
+@implementation KETableView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
